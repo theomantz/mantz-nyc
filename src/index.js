11de511a57a0a0140e31dbd2c94a3f9ab601f95d
theo m <theo@mantz.io>
2021-10-29 21:12:53 -0400

    Change hashrouter to browserrouter

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from "react-dom";
 import "./reset.css";
 import "./index.css";
 import App from "./App";
-import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <Switch>
         <Route exact path="/" component={App} />
         <Redirect path="*" to="/" />
       </Switch>
-    </HashRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
